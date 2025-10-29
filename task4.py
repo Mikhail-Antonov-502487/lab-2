8e945db456fba62c7ef0f83e2c46993b1d8f7830
Antonov Mikhail <mixail.antonoff2018@yandex.ru>
2025-10-29 20:06:00 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,6 +1,7 @@
 # извлечение данных
 import xml.etree.ElementTree as ET
 
+
 tree = ET.parse('currency.xml')
 root = tree.getroot()
 
@@ -11,6 +12,6 @@ for valute in root.findall('Valute'):
     if valute.find('Nominal').text == "10":
         charcod_vector.append(valute.find('CharCode').text)
     if valute.find('Nominal').text == "100":
-        charcod_vector.append(valute.find('CharCode').text)
+        charcod_vector.append(valute.find('CharCode').text
 
 print("CharCode:", charcod_vector)
